7679609a7d74fa3896c1a681509589734a689622
Brad Huber <bhuber@iseatz.com>
2013-05-01 13:43:09 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NifL::Application.config.secret_token = '209117dcf062f7f560373d248c1a17814af650e1bc547d1822b167f4e02d0a255d31a9a92b1359d4d56bc39acd7031bfc890cbdb6c922b5a98f7c262efe11430'
